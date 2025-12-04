030cfc1079bade4ce0dc19e6d053158e5bedc022
Birgoan George <georgebirgoan@gmail.com>
2025-12-04 19:22:29 +0200

    --ADD ENV PROD

--- src/modules/auth/login/login.service.ts
+++ src/modules/auth/login/login.service.ts
@@ -14,6 +14,7 @@ import { SessionData } from '../types/sessionInterface';
 export class LoginService {
 
 
+  
   constructor(
     private prisma:PrismaService,
     private userAuthService:UserAuthService,
